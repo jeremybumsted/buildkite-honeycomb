aa5a8d73f844c5fa73b33867fc93d141f1b7c5e9
Jeremy Bumsted <jeremy@buildkite.com>
2025-06-19 17:16:28 -0600

    last cleanup to get spans sorted within the build trace

--- .buildkite/scripts/buildevents_go_test.sh
+++ .buildkite/scripts/buildevents_go_test.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+STEP_START=$(date +%s)
+STEP_SPAN_ID="${BUILDKITE_JOB_ID}-command"
+
 # clean up the test cache
 buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID go-clean -- go clean -testcache
 buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID go-test -- gotestsum --format testname --junitfile unit-tests.xml --junitfile-testcase-classname relative -- -coverprofile=cover.out ./...
