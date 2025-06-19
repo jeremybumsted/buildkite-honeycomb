e2c564f939dd5721ee1423409908e73788a6dc7d
Jeremy Bumsted <jeremy@buildkite.com>
2025-06-19 16:54:08 -0600

    send test output to STDOUT still

--- .buildkite/scripts/buildevents_go_test.sh
+++ .buildkite/scripts/buildevents_go_test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # clean up the test cache
 buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID go-clean -- go clean -testcache
-if [[ ! $(buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID go-test -- gotestsum --format testname --junitfile unit-tests.xml --junitfile-testcase-classname relative -- -coverprofile=cover.out ./...) ]]; then
+buildevents cmd $BUILDKITE_BUILD_ID $STEP_SPAN_ID go-test -- gotestsum --format testname --junitfile unit-tests.xml --junitfile-testcase-classname relative -- -coverprofile=cover.out ./...
+TEST_EXIT_CODE=$?
+
+if [[ $TEST_EXIT_CODE -ne 0 ]]; then
     buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START command
     exit 1
+else
+    buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START command
 fi
-
-buildevents step $BUILDKITE_BUILD_ID $STEP_SPAN_ID $STEP_START command
